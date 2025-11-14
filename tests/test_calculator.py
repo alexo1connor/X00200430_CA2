95a0112bfc326c9eedd40558e4555394dbdf3975
x00200430 <x00200430@mytudublin.ie>
2025-11-14 20:36:45 +0000

    Testing code coverage fail

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,12 +7,4 @@ class TestCalculator:
         assert add(-1, 1) == 0
         assert add(-1, -1) == -2
     
-    def test_subtract(self):
-        assert subtract(5, 3) == 2
-        assert subtract(0, 0) == 0
-        assert subtract(-1, -1) == 0
     
-    def test_multiply(self):
-        assert multiply(2, 3) == 6
-        assert multiply(-1, 1) == -1
-        assert multiply(-1, -1) == 1
